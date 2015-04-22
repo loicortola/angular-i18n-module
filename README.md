0b2bee7436956fc81dd64e31a2a0d25b7648b0e6
Loïc Ortola <contact@loicortola.com>
2015-04-22 00:37:34 -0300

    Fixed performance and design issues, fixed Angular 1.3 compatibility, implemented event and promise hooks

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 A simple to use and effective AngularJS library to translate your app
 
-This repo is for distribution on `bower`. 
+This repo is for distribution on `bower`.
 Please file issues and pull requests against that repo.
 
 # Install
@@ -22,8 +22,8 @@ Add a `<script>` to your `index.html`:
 <script src="/bower_components/angular-i18n-module/angular-i18n-module.js"></script>
 ```
 
-Add `i18n` as a dependency for your app in your main Angular app.js, and configure your locale resources:  
-__NB: You need to put at least one resource named "default"__  
+Add `i18n` as a dependency for your app (in the app module declaration line) in your main Angular app.js, and configure your locale resources:
+__NB: You need to put at least one resource named "default"__
 ```javascript
 var app = angular.module('app', ['myModule1', 'myModule2 ', 'myModule3', 'i18n']);
 
@@ -39,12 +39,12 @@ var i18n = angular.module('i18n');
   }]);
 ```
 
-Simply inject the angular i18nService in any of your Angular controllers:  
+Simply inject the angular i18nService in any of your Angular controllers:
 ```javascript
 app.controller('MainCtrl', ['$scope', 'i18nService', function MainCtrl($scope, i18nService) {
     //Your controller code here
 }]);
-```  
+```
 
 # Documentation
 ---------------------
@@ -55,7 +55,7 @@ app.controller('MainCtrl', ['$scope', 'i18nService', function MainCtrl($scope, i
 * Handle conditional messages
 * Handle multi parameter messages
 * Handle messages with html content
-* Only load the current language  
+* Only load the current language
 * Easy asynchronous locale load
 * Directive to enable any element to change the locale
 
@@ -74,8 +74,8 @@ app.controller('MainCtrl', ['$scope', 'i18nService', function MainCtrl($scope, i
 
 **returns** The locale resource if the resource exists, the key string otherwise
 
-***Example***  
-`var myString = getString('label.hello.world');`  
+***Example***
+`var myString = getString('label.hello.world');`
 `var myString2 = getString('message.game.over',36532,'Loic');`
 
 ####isLocaleEmpty()
@@ -90,14 +90,13 @@ app.controller('MainCtrl', ['$scope', 'i18nService', function MainCtrl($scope, i
 | __language__ | String representing the desired language. Should match the format of the __RFC 3066__ specification |
 
 
-If there is a locale resource with a name matching the parameter (**ex:** *example_locale_XX.json* or *other_example_XX_YY.json*), 
-the matching locale will be loaded **asynchronously**, and will call **$digest()** to recompute the entries
-
+If there is a locale resource with a name matching the parameter (**ex:** *example_locale_XX.json* or *other_example_XX_YY.json*),
+the matching locale will be loaded **asynchronously**, and will recompute the entries in a **$digest()**.
 Otherwise, the fallback will be to load the locale with the key '**default**'
-
-***Example***  
-`selectLanguage('en_US');`  
-
+N.B.: The selectLanguage method returns a promise allowing you to add custom behaviours in case of success, error, or after the operation has returned.
+***Example***
+`selectLanguage('en_US');`
+`selectLanguage('en_US').then(function(locale) {console.log('locale ' + locale + 'was loaded successfully');})};`
 
 ####setLocales( locales , select )
 
@@ -106,9 +105,10 @@ Otherwise, the fallback will be to load the locale with the key '**default**'
 | __locales__ | A JSON object with *'key': 'value'* pairs. **key** being the __RFC 3066__ string representing the language (or **default**), and **value** the path of the json resource file |
 | __select__   | Boolean (optional)                                                                                                                                                           |
 
-Sets the custom locales **into** the **i18n** module.  
-If the second argument is set and is true, it triggers the selectLanguage() method with the default environment language of the user (browsers language)                                                  
-***Example***  
+Sets the custom locales **into** the **i18n** module.
+If the second argument is set and is true, it triggers the selectLanguage() method with the default environment language of the user (browsers language).  
+This method should not be called by anything else than the Service Provider.
+***Example***
 ```
 setLocales({
       'default': '../i18n/resources-locale_en_US.json',
@@ -125,7 +125,7 @@ setLocales({
 
 Consider the following json files
 
-**resources\_locale\_en\_US.json**  
+**resources\_locale\_en\_US.json**
 ```json
 {
   "label.hello.world": "Hello world!",
@@ -140,11 +140,11 @@ Consider the following json files
     "true": "We are glad you liked the story",
     "false": "So sorry to hear you did not like the story!"
   },
-  "message.with.html": "I <b>LOVE</b> "rock & roll" so <i>(put the rest here...)</i>"
+  "message.with.html": "I <b>LOVE</b> \"rock & roll\" so <i>(put the rest here...)</i>"
 }
 ```
 
-**resources\_locale\_fr.json**  
+**resources\_locale\_fr.json**
 ```json
 {
   "label.hello.world": "Salut le monde!",
@@ -164,87 +164,87 @@ Consider the following json files
 
 ###B. Use in html views
 
-####1. With angular {{ }} statements  
+####1. With angular {{ }} statements
 
-***Basic call***  
+***Basic call***
 ```
 {{'label.hello.world' | i18n}}
-```    
-*==>  Hello world!*  
+```
+*==>  Hello world!*
 
-***Count call***  
-```  
+***Count call***
+```
 {{'label.users.connected' | i18n:1}}
-```  
-*==>  You currently have one (that means only 1) user connected*   
+```
+*==>  You currently have one (that means only 1) user connected*
 ```
 {{'label.users.connected' | i18n:12}}
-```  
-*==>  You have 12 users connected*   
+```
+*==>  You have 12 users connected*
 ```
 {{'label.users.connected' | i18n:0}}
-```  
-*==>  You have no users connected*  
+```
+*==>  You have no users connected*
 
-***Named parameter call***  
+***Named parameter call***
 ```
 {{'message.story.beginning' | i18n:{'firstCharacter':'John', 'secondCharacter':'Paul', 'thirdCharacter':'Matthew'} }}
-```  
-*==>  This is the story of John who called his friend Paul to tell him if he could give Matthew a ride to the airport. Problem is, Paul's phone was on voicemail, and John did not have a car.*   
+```
+*==>  This is the story of John who called his friend Paul to tell him if he could give Matthew a ride to the airport. Problem is, Paul's phone was on voicemail, and John did not have a car.*
 
-***Generic parameter call***  
+***Generic parameter call***
 ```
 {{ 'message.story.end' | i18n:'John':'Paul':'Matthew' }}
-```  
-*==>  So John told Matthew that he would have to drive there by himself, and apologized on behalf of Paul*   
+```
+*==>  So John told Matthew that he would have to drive there by himself, and apologized on behalf of Paul*
 
-***Conditional call***  
+***Conditional call***
 ```
 {{'message.feedback' | i18n:true }}
-```  
-*==>  We are glad you liked the story*   
+```
+*==>  We are glad you liked the story*
 ```
 {{'message.feedback' | i18n:false }}
-```  
-*==>  So sorry to hear you did not like the story!*  
+```
+*==>  So sorry to hear you did not like the story!*
 
-***Call to non existing entry returns the key***  
+***Call to non existing entry returns the key***
 ```
 {{'this.doesnt.exist' | i18n }}
-```  
-*==>  this.doesnt.exist*  
+```
+*==>  this.doesnt.exist*
 
-***WARNING: HTML tags cannot be processed the same way as regular strings! See section 2. below***  
+***WARNING: HTML tags cannot be processed the same way as regular strings! See section 2. below***
 ```
 {{'message.with.html' | i18n }}
-```  
-*==>  I \<b\>LOVE\</b\> "rock & roll" so \<i\>(put the rest here...)\</i\>*  
+```
+*==>  I \<b\>LOVE\</b\> "rock & roll" so \<i\>(put the rest here...)\</i\>*
 
 
 
-####2. With ng-bind-html directive (for resources with HTML content)  
+####2. With ng-bind-html directive (for resources with HTML content)
 ```html
 <span ng-bind-html="'message.with.html' | i18n"></span>
-```    
-*==>  I __LOVE__ "rock & roll" so _(put the rest here...)_*   
+```
+*==>  I __LOVE__ "rock & roll" so _(put the rest here...)_*
 
 
 ###C. Use in js files
 
 **NB:** *First, you need to inject the i18nService into your angular controller, or service, or filter etc...
 Then a simple call to __getString()__ will give you what you need*
-
 ```javascript
 var myStoryBeginning = i18nService.getString(
     'message.story.beginning',
     {
-        'firstCharacter':'John', 
-        'secondCharacter':'Paul', 
+        'firstCharacter':'John',
+        'secondCharacter':'Paul',
         'thirdCharacter':'Matthew'
     });
 var myStoryEnd = i18nService.getString('message.story.end','John','Paul','Matthew');
 var outputMessage = i18nService.getString('message.feedback',true);
 ```
+Warning: The previous statement is computing a VALUE, therefore no watch is being made in case you decide to change the language.
 
 ###D. Add locale-switching buttons
 
@@ -255,10 +255,14 @@ Simply call the directive as a parameter for any element of your html template.
 <button i18n-language-selector="'en'">Click to switch to english</button>
 ```
 
+###E. OnLocaleLoaded event
+
+To create your own hooks after a locale is successfully loaded, simply listen to the event named 'i18nService.onLocaleLoaded', which is fired on the rootScope.
+
 # License
 ---------------------
 
-   Copyright 2014 Loïc Ortola
+   Copyright 2015 Loïc Ortola
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
--- angular-i18n-module.js
+++ angular-i18n-module.js
@@ -16,9 +16,13 @@
 
 var i18nFilter = function (i18nService) {
 
-    return function () {
-        return i18nService.getString(arguments);
-    }
+  var filter = function () {
+    return i18nService.getString(arguments);
+  }
+
+  filter.$stateful = true;
+
+  return filter;
 
 }
 
@@ -26,162 +30,190 @@ var i18nFilter = function (i18nService) {
 var i18nModule = angular.module('i18n', ['ngCookies', 'ngSanitize']);
 
 
+var I18N = function ($cookieStore, $http, $q, $window, $sce, $rootScope, config) {
+
+  this.config = config;
+
+  this.$cookieStore = $cookieStore;
+  this.$http = $http;
+  this.$q = $q;
+  this.$window = $window;
+  this.$sce = $sce;
+
+
+  /* Init i18n Attributes:
+   * language: Retrieve the previously stored language. If none, retrieve language of navigator
+   * locales: the locales provided within the config object (service provider)
+   * dictionary: the current dictionary
+   */
+  this.i18n.language = $cookieStore.get('locale') || $window.navigator.userLanguage || $window.navigator.language;
+  this.i18n.locales = config.locales;
+  this.i18n.dictionary = [];
+  this.selectLanguage(this.i18n.language);
+
+
+  $rootScope.$watch((function() { return this.i18n.loaded}).bind(this), function(newVal,oldVal) {
+    console.log("watched "+newVal);
+  });
+
+
+}
+
+// Prototype Data
+I18N.prototype.i18n = {
+    language: null,
+    dictionary: this.dictionary,
+    loaded: false,
+    locales: null
+  };
+
+// Prototype Resource loading method
+  I18N.prototype.loadResources = function (url) {
+    var deferred = this.$q.defer();
+    this.$http.get(url)
+        .success((function (data) {
+          this.i18n.dictionary = data;
+          this.i18n.loaded = true;
+          console.debug('i18n: locale successfully loaded');
+          deferred.resolve();
+        }).bind(this))
+        .error((function () {
+          console.error('i18n: Cannot load locale');
+          deferred.reject();
+        }).bind(this));
+    return deferred.promise;
+  };
+
+  // Prototype Language selection method
+  I18N.prototype.selectLanguage = function (language) {
+
+    this.i18n.language = language;
+
+    console.debug("i18n: Selected language:", this.i18n.language);
+
+    // Load selected locale or default if none
+    // example:
+    // browser language: en_US
+    // match "en_US" in locales? no
+    // > match "en" in locales? yes
+    // (otherwise, default locale will be returned)
+    var url = this.i18n.locales['default'];
+
+    if (this.i18n.language in this.i18n.locales) {
+      url = this.i18n.locales[this.i18n.language];
+    }
+    else {
+      if (this.i18n.language.substr(0, 2) in this.i18n.locales) {
+        url = this.i18n.locales[this.i18n.language.substr(0, 2)];
+      }
+      else {
+        console.log("i18n: Did not find a matching locale resource. Falling back to default");
+      }
+    }
+
+    // Save language selection to client's cookie
+    this.$cookieStore.put('locale', this.i18n.language);
+
+    return this.loadResources(url)
+               .then(function () {
+                  $rootScope.$emit('i18nService.onLocaleLoaded', this.i18n.language);
+               });
+  }
+
+  // Prototype Replace function for parametered localized strings
+  I18N.prototype.replaceArgs = function (val, isConditional, args) {
+    var multiParams = false;
+    // If we have a plural/conditional string definition, we need to handle the {n} replacement
+    if (args.length > 1)
+      if (isConditional) {
+        val = val.replace('{}', args[1]);
+        if (args.length > 2)
+          multiParams = true;
+      }
+      else if (args.length > 1)
+        multiParams = true;
+
+    // Parameters browse for either generic or named parameters
+    for (var i = isConditional ? 2 : 1; i < args.length; i++) {
+      // For named parameters
+      if (args[i] instanceof Object)
+        for (var p in args[i])
+          val = val.replace(new RegExp('\\{' + p + '\\}', 'g'), args[i][p]);
+      // For generic parameters, either multi params or not
+      else if (multiParams) {
+        var toReplace = '\\{' + (isConditional ? (i - 1) : i) + '\\}';
+        val = val.replace(new RegExp(toReplace, 'g'), args[i]);
+      }
+      else
+        val = val.replace(new RegExp('\\{\\}', 'g'), args[i]);
+    }
+    return val;
+  }
+
+  // Computed local string retrieval method
+  I18N.prototype.getString = function (args) {
+    var input = args[0];
+    if (!(args instanceof Object))
+      input = args;
+
+    if (this.i18n.loaded && input in this.i18n.dictionary) {
+      var val = this.i18n.dictionary[input];
+      // For plural/conditional separated entries
+      if (val instanceof Object) {
+        if (val.hasOwnProperty('zero') && (!args[1] || args[1] == 0))
+          val = val['zero'];
+        else if (val.hasOwnProperty('one') && args[1] == 1)
+          val = val['one'];
+        else if (val.hasOwnProperty('true') && args[1])
+          val = val['true'];
+        else if (val.hasOwnProperty('false') && !args[1])
+          val = val['false'];
+        else if (val.hasOwnProperty('default'))
+          val = val['default'];
+        else
+          console.error("i18n: You need to provide at least a 'default' entry in your lang resources for the conditional property " + input);
+        return this.replaceArgs(val, true, args);
+      }
+      // String replace if arguments supplied
+      return this.$sce.trustAsHtml(this.replaceArgs(val, false, args));
+    }
+    else {
+      return input;
+    }
+  }
+
+  // IsLocaleEmpty to test if locales were set or not
+  I18N.prototype.isLocaleEmpty = function () {
+    return this.i18n.locales ? false : true;
+  }
+
+
 var i18nService = function () {
 
-    /*
-     Internationalization i18n container
-     language: Retrieve the previously stored language. If none, retrieve language of navigator
-     locales:  Declare all locale resource file URI-path, including mandatory "default" entry
-     Example:
-     locales: {
-     'default': '../i18n/resources-locale_en_US.json',
-     'en': '../i18n/resources-locale_fr.json'
-     }
-     */
-    this.language = null;
-    this.dictionary = [];
-    this.loaded = false;
-    this.locales = null;
+  /*
+   i18n config from service provider
+   locales:  Declare all locale resource file URI-path, including mandatory "default" entry
+   Example:
+   locales: {
+   'default': '../i18n/resources-locale_en_US.json',
+   'en': '../i18n/resources-locale_fr.json'
+   }
+   */
+  this.config = {
+    locales: null
+  };
 
-    //SetLocales to init the library
-    this.setLocales = function (locales) {
-        this.locales = locales;
-    };
+  //SetLocales to init the library
+  this.setLocales = function (locales) {
+    this.config.locales = locales;
+  };
 
-    this.$get = function ($cookieStore, $http, $window, $sce) {
+  this.$get = ['$rootScope', '$cookieStore', '$http', '$q', '$window', '$sce', function ($rootScope, $cookieStore, $http, $q, $window, $sce) {
 
-        var i18nService = {
-            i18n: {
-                language: null,
-                dictionary: this.dictionary,
-                loaded: false,
-                locales: null
-            },
+    return new I18N($cookieStore, $http, $q, $window, $sce, $rootScope, this.config);
 
-            // Resource loading method
-            loadResources: function (url) {
-                $http.get(url)
-                    .success(function (data) {
-                        i18nService.i18n.dictionary = data;
-                        i18nService.i18n.loaded = true;
-                        console.debug('i18n: locale successfully loaded');
-                    })
-                    .error(function () {
-                        console.error('i18n: Cannot load locale');
-                    });
-            },
+  }];
 
-            // Language selection method
-            selectLanguage: function (language) {
-
-                i18nService.i18n.language = language;
-
-                console.info("i18n: Selected language:", i18nService.i18n.language);
-
-
-                // Load selected locale or default if none
-                // example:
-                // browser language: en_US
-                // match "en_US" in locales? no
-                // > match "en" in locales? yes
-                // (otherwise, default locale will be returned)
-                var url = i18nService.i18n.locales['default'];
-
-                if (i18nService.i18n.language in i18nService.i18n.locales) {
-                    url = i18nService.i18n.locales[i18nService.i18n.language];
-                }
-                else {
-                    if (i18nService.i18n.language.substr(0, 2) in i18nService.i18n.locales) {
-                        url = i18nService.i18n.locales[i18nService.i18n.language.substr(0, 2)];
-                    }
-                    else {
-                        console.log("i18n: Did not find a matching locale resource. Falling back to default");
-                    }
-                }
-
-                // Save language selection to client's cookie
-                $cookieStore.put('locale', i18nService.i18n.language);
-
-                i18nService.loadResources(url);
-            },
-
-            // Replace function for parametered localized strings
-            replaceArgs: function (val, isConditional, args) {
-                var multiParams = false;
-                // If we have a plural/conditional string definition, we need to handle the {n} replacement
-                if (args.length > 1)
-                    if (isConditional) {
-                        val = val.replace('{}', args[1]);
-                        if (args.length > 2)
-                            multiParams = true;
-                    }
-                    else if (args.length > 1)
-                        multiParams = true;
-
-                // Parameters browse for either generic or named parameters
-                for (var i = isConditional ? 2 : 1; i < args.length; i++) {
-                    // For named parameters
-                    if (args[i] instanceof Object)
-                        for (var p in args[i])
-                            val = val.replace(new RegExp('\\{' + p + '\\}', 'g'), args[i][p]);
-                    // For generic parameters, either multi params or not
-                    else if (multiParams) {
-                        var toReplace = '\\{' + (isConditional ? (i - 1) : i) + '\\}';
-                        val = val.replace(new RegExp(toReplace, 'g'), args[i]);
-                    }
-                    else
-                        val = val.replace(new RegExp('\\{\\}', 'g'), args[i]);
-                }
-                return val;
-            },
-            // Computed local string retrieval method
-            getString: function (args) {
-                var input = args[0];
-                if (!(args instanceof Object))
-                    input = args;
-
-                if (i18nService.i18n.loaded && input in i18nService.i18n.dictionary) {
-                    var val = i18nService.i18n.dictionary[input];
-                    // For plural/conditional separated entries
-                    if (val instanceof Object) {
-                        if (val.hasOwnProperty('zero') && (!args[1]  || args[1] == 0))
-                            val = val['zero'];
-                        else if (val.hasOwnProperty('one') && args[1] == 1)
-                            val = val['one'];
-                        else if (val.hasOwnProperty('true') && args[1])
-                            val = val['true'];
-                        else if (val.hasOwnProperty('false') && !args[1])
-                            val = val['false'];
-                        else if (val.hasOwnProperty('default'))
-                            val = val['default'];
-                        else
-                            console.error("i18n: You need to provide at least a 'default' entry in your lang resources for the conditional property " + input);
-                        return this.replaceArgs(val, true, args);
-                    }
-                    // String replace if arguments supplied
-                    return $sce.trustAsHtml(this.replaceArgs(val, false, args));
-                }
-                else {
-                    return input;
-                }
-            },
-
-            //IsLocaleEmpty to test if locales were set or not
-            isLocaleEmpty: function () {
-                var locale = i18nService.i18n.locales;
-                return i18nService.i18n.locales ? false : true;
-            }
-
-        }
-
-        //Init
-        i18nService.i18n.language = this.language || $cookieStore.get('locale') || $window.navigator.userLanguage || $window.navigator.language;
-        i18nService.i18n.locales = this.locales;
-        i18nService.i18n.dictionary = this.dictionary;
-        i18nService.selectLanguage(i18nService.i18n.language);
-
-        return i18nService;
-    }
 }
 
 /** Provider declaration */
@@ -191,30 +223,30 @@ i18nModule.provider('i18nService', i18nService);
 i18nModule.filter('i18n', ['i18nService', i18nFilter]);
 
 /** Directive declaration */
-i18nModule.directive('i18nLanguageSelector', ['$compile', 'i18nService', function($compile, service){
-       return {
-           restrict: 'A',
-           replace: false,
-           terminal: true,
-           priority: 1000,
-           scope: {
-               "i18nLanguageSelector": '='
-           },
-           compile: function compile(element, attrs){
-             element.attr('ng-click', 'select()');
-             element.removeAttr('i18n-language-selector');
-             element.removeAttr('data-i18n-language-selector');
-                return {
-                    pre: function preLink(scope, iElement, iAttrs, controller){
-                    },
-                    post: function postLink(scope, iElement){
-                        scope.select = function() {
-                            service.selectLanguage(scope.i18nLanguageSelector);
-                        }
-                        $compile(iElement)(scope);
-                    }
-                };
-           }
+i18nModule.directive('i18nLanguageSelector', ['$compile', 'i18nService', function ($compile, service) {
+  return {
+    restrict: 'A',
+    replace: false,
+    terminal: true,
+    priority: 1000,
+    scope: {
+      "i18nLanguageSelector": '='
+    },
+    compile: function compile(element, attrs) {
+      element.attr('ng-click', 'select()');
+      element.removeAttr('i18n-language-selector');
+      element.removeAttr('data-i18n-language-selector');
+      return {
+        pre: function preLink(scope, iElement, iAttrs, controller) {
+        },
+        post: function postLink(scope, iElement) {
+          scope.select = function () {
+            service.selectLanguage(scope.i18nLanguageSelector);
+          }
+          $compile(iElement)(scope);
+        }
+      };
+    }
 
-       }
+  }
 }]);
